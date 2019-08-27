6e182957a8d6bad6ebe737eee581eaa63b7bdd95
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-27 23:04:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   
   
   def add_student(student_name, grade)
-    @roster[grade] ||= []      #if @roster[grade] exists, leave alone. otherwise set 
+    @roster[grade] ||= []   #if @roster[grade] exists, leave alone. Otherwise set to []
     @roster[grade] << student_name
   end
 end
