6d94d306520e4e703d3e0925cd8c1984b759af5e
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-27 22:54:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,9 @@ class School
   
   def add_student(student_name, grade)
     if @roster[grade]
-    @roster[grade]= []
-    @roster[grade] << student_name
+    else
+      @roster[grade]= []
+      @roster[grade] << student_name
+    end
   end
 end
