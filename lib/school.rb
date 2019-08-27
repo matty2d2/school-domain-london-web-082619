80f616e75f419c41c5e4a5348c7a250f2dfbacea
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-27 22:29:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+  
+  def add_student(student_name, grade)
+    @roster[grade]= []
+    @roster[grade] << student_name
+  end
+end
