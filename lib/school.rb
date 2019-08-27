41da020a1dac8892d4cd99b13527c7e316afb16c
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-27 23:09:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,13 +3,11 @@ class School
   
   def initialize(name)
     @name = name
-    @roster = Hash.new{}
+    @roster = Hash.new{|h,k| h[k]=[]}
   end
   
   
   def add_student(student_name, grade)
-    @roster[grade] ||= []   
-      #if @roster[grade] exists, leave alone. Otherwise set to []
     @roster[grade] << student_name
   end
 end
